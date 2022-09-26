e9a6a1664d61132804ee72b8555f4c4d0cc6dd50
Maxwell Okoye <itomax19@gmail.com>
2022-09-26 15:18:47 +0100

    Add HTML and CSS

--- app.js
+++ app.js
@@ -1,1 +1,35 @@
 alert("So, you would love to travel to SPACE!");
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+
+// functionmyFunction() {
+//     document.getElementById("myDropdown1").classList.toggle("show");
+// }
+
+// window.onclick = function (event) {
+//     if (!event.target.matches(".dropbtn1")) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content1");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown1 = dropdowns{i};
+//             if (openDropdown1.classList.contains("show")) {
+//                 openDropdown1.classList.remove("show");
+//             }
+//         }
+//     }
+// };
